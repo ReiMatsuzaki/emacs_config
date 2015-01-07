9125423b7183e9dd12442700ab3429d924506639
Rei Matsuzaki (rccls) <matsuzaki.rei@sepia.chem.keio.ac.jp>
2015-01-07 13:58:01 +0900

    write primitive STO class code

--- src/ao/sto.hpp
+++ src/ao/sto.hpp
@@ -3,8 +3,8 @@
 
 /**
  * @file sto.hpp
- * @brief Radial Slater type orbital basis.
- * @date 2014/1/6 new
+ * @brief Normalized primitive Radial Slater type orbital basis.
+ * @date 2014/1/7 new
  * @author Rei matsuzaki
  */
 
@@ -23,18 +23,16 @@ using namespace StongMath;
 // ** start class
 
 /**
- * radial slater type orbital basis.
+ * normalized primitive radial slater type orbital basis.
  *
  */
 template<class F>
 class STO {
 
-private:
-  unsigned int num;    /** number of primitive STOs. */
-  F*           zs;     /** orbital exponent array (size = num)*/
-  F*           cs;     /** coefficient array (size = num) */
-  int*         pns;    /** principle number array (size = num) */
-  F*           ns;     /** normalized constant array (size = num) */
+private:  
+  F           z;     /** orbital exponent*/
+  int         pn;    /** principle number  */
+  F           n;     /** normalized constant */
 
 // * Member      
 // ** constructor/destructor
@@ -43,25 +41,14 @@ private:
 public:
   /**
    * Constructor
-   * @param _num number of primitive STOs.
    */
-  STO(unsigned int _num) :
-    num(_num) {
-    zs  = new F[num];
-    cs  = new F[num];
-    pns = new int[num];
-    ns  = new F[num];
-  }
+  STO() {}
 
 
 // *** Destructor
-
+  
   /** Destructor */
   ~STO() {
-    delete[] zs;
-    delete[] cs;
-    delete[] pns;
-    delete[] ns;
   }
   
 
@@ -69,65 +56,45 @@ public:
 // ** accessor
 // *** getter
   
-  /** get number of term */
-  const unsigned int Get_num() { return num; }
+  /** get zeta */
+  const F Get_z() { return z; }
 
-  /** get i th term of zs */
-  const F Get_zs_i(unsigned int i ) { return zs[i]; }
+  /** get principle number  */
+  const int Get_pn() { return pn; }
 
-  /** get i th term of cs */
-  const F Get_cs_i(unsigned int i ) { return cs[i]; }
-
-  /** get i th term of pns */
-  const int Get_pns_i(unsigned int i) { return pns[i]; }
-
-  /** get i th term of ns */
-  const F Get_ns_i(unsigned int i) { return ns[i]; }
+  /** get normalization constant */
+  const F Get_n() { return n; }
   
   
 // *** setter
-
-
-
+  
 public:  
-  //** set zs without normalization*/  
-  void Set_zs_without_normalization(const F* _zs) {
-    for(unsigned int i = 0; i < num; i++) 
-      zs[i]= _zs[i];
+  //** set z without normalization*/  
+  void Set_z_without_normalization(F _zs) {
+    z = _zs;
     }
 
   //** set pns without normalization*/
-  void Set_pns_without_normalization(const int* _pns) {
-    for(unsigned int i = 0; i < num; i++) 
-      pns[i]= _pns[i];  
+  void Set_pn_without_normalization(int _pn) {
+    pn = _pn;
   }
   
   //** set zs with normalization*/
-  void Set_zs(const F* _zs) {
-    for(unsigned int i = 0; i < num; i++) {
-      zs[i]= _zs[i];
-      Compute_ns_i(i);
-    }
+  void Set_z(F _z) {
+    z = _z;
+    Normalize();
   }
 
-  //** set cs*/
-  void Set_cs(const F* _cs) {
-    for(unsigned int i = 0; i < num; i++)
-      cs[i]= _cs[i];
+  //** set pn with normalization*/
+  void Set_pn(int _pn) {
+    pn = _pn;
+    Normalize();
   }
 
-  //** set pns with normalization*/
-  void Set_pns(const int* _pns) {
-    for(unsigned int i = 0; i < num; i++) {
-      pns[i]= _pns[i];
-      Compute_ns_i(i);
-    }
-  }
-
-  //** compute and store normalization constant */
-  void Compute_ns_i(unsigned int i) {
-    F norm2 = STO_int(2 * pns[i], 2 * zs[i]);
-    ns[i] = sqrt(norm2);
+   //** compute and store normalization constant */
+  void Normalize() { 
+    F norm2 = STO_int(2 * pn, 2 * z);
+    n = sqrt(norm2);
   }
   
 // ** IO
@@ -154,37 +121,16 @@ public:
 // ** Factory (static)
 // *** different principle number
 
-  /** compute linear combination of normalized STOs
-   *  with different principle numbers.
-   */
   
-  static STO* NormalizedSTOs(int _num, F* _zs, F* _cs, int* _pns) {
+  /** compute normalized STOs */
+  static STO* New_NormalizedSTO(F _z, int _pn) {
 
-    STO* sto = new STO(_num);
-    sto->Set_zs_without_normalization(_zs);
-    sto->Set_cs(_cs);
-    sto->Set_pns(_pns);
-
+    STO* sto = new STO();
+    sto->Set_z_without_normalization(_z);
+    sto->Set_pn_without_normalization(_pns);
+    sto->Normalize();
     return sto;
-  }
-
-// *** same principle number
-
-  /** compute linear combination of normalized STOs
-   *  with same principle numbers.
-   */
-  static STO* NormalizedSTOs(int _num, F* _zs, F* _cs, int _pn) {
-
-    int* _pns = new int[_num];
-
-    for(unsigned int i = 0; i < _num; i++)
-      _pns[i] = _pn;
-
-    STO<F>* ptr = STO<F>::NormalizedSTOs(_num, _zs, _cs, _pns);
-
-    delete[] _pns;
-
-    return (ptr);
+    
   }
 
 
