6bf35b6e76c4bdff8912259457e9ecdcf381168c
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2015-03-13 09:47:37 +0900

    separate window move functions to new file named move.el from gui.el.

--- gui.el
+++ gui.el
@@ -13,40 +13,17 @@
 
 ;;;  Migemo
 
-(require 'migemo)
-(setq migemo-command "cmigemo")
-(setq migemo-options '("-q" "--emacs"))
+(when (locate-library "migemo")
+  (progn
+    (require 'migemo)
+    (setq migemo-command "cmigemo")
+    (setq migemo-options '("-q" "--emacs"))
 
-(setq migemo-user-dictionary nil)
-(setq migemo-regex-dictionary nil)
-(setq migemo-coding-system 'utf-8-unix)
-(load-library "migemo")
-(migemo-init)
-
-;;;  move     
-
-(defvar move-global-minor-mode-map
-  (let ((map (make-sparse-keymap)))
-    (setq windmove-wrap-around t)
-    (define-key map (kbd "M-j") 'windmove-down)
-    (define-key map (kbd "M-k") 'windmove-up)
-    (define-key map (kbd "M-l") 'windmove-right)
-    (define-key map (kbd "M-h") 'windmove-left)
-    (define-key map (kbd "M-h") 'windmove-left)
-    (define-key map (kbd "M-J") 'buffer-flip-down)
-    (define-key map (kbd "M-K") 'buffer-flip-up)
-    (define-key map (kbd "M-L") 'buffer-flip-right)
-    (define-key map (kbd "M-H") 'buffer-flip-left)
-    (define-key map (kbd "M-s") 'save-opening-buffer)
-    (define-key map (kbd "M-m") 'switch-to-opening-buffer)
-    map))
-
-(define-minor-mode move-global-minor-mode
-  "move minor mode"
-  :global t)
-
-(move-global-minor-mode t)
-
+    (setq migemo-user-dictionary nil)
+    (setq migemo-regex-dictionary nil)
+    (setq migemo-coding-system 'utf-8-unix)
+    (load-library "migemo")
+    (migemo-init)))
 
 
 
---
+++ move.el
@@ -0,0 +1,25 @@
+;;;  move     
+
+(defvar move-global-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (setq windmove-wrap-around t)
+    (define-key map (kbd "M-j") 'windmove-down)
+    (define-key map (kbd "M-k") 'windmove-up)
+    (define-key map (kbd "M-l") 'windmove-right)
+    (define-key map (kbd "M-h") 'windmove-left)
+    (define-key map (kbd "M-h") 'windmove-left)
+    (define-key map (kbd "M-J") 'buffer-flip-down)
+    (define-key map (kbd "M-K") 'buffer-flip-up)
+    (define-key map (kbd "M-L") 'buffer-flip-right)
+    (define-key map (kbd "M-H") 'buffer-flip-left)
+    (define-key map (kbd "M-s") 'save-opening-buffer)
+    (define-key map (kbd "M-m") 'switch-to-opening-buffer)
+    map))
+
+(define-minor-mode move-global-minor-mode
+  "move minor mode"
+  :global t)
+
+(move-global-minor-mode t)
+
+
